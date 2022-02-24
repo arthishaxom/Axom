074ed650d8e854afe9a377a9d158691a13dffa19
arthishaxom <arthishgfx@gmail.com>
2022-02-24 15:58:28 +0530

    Fixed System Stats

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,10 +1,71 @@
-from discord.ui import Button,View
+from discord import ui
+from discord.ext import menus
 import discord
 from discord.ext import commands
-import json
+import psutil
 
-import requests
+class MySource(menus.ListPageSource):
+    async def format_page(self, menu, entries):
+        serverlist = []
+        i = 0
+        for guild in self.client.guilds:
+            i+=1
+            e = f"` {str(i)} ` - {guild.name} : `{guild.id}`"
+            serverlist.append(e)
 
+        total_servers = len(serverlist)
+        embed = discord.Embed(
+            title = f"{total_servers}",
+            description=f"{entries}", 
+            color=discord.Colour.gold())
+        embed.set_footer(text=f"Requested by {menu.ctx.author}")
+        return embed
+
+class MyMenuPages(ui.View, menus.MenuPages):
+    def __init__(self, source):
+        super().__init__(timeout=60)
+        self._source = source
+        self.current_page = 0
+        self.ctx = None
+        self.message = None
+
+    async def start(self, ctx, *, channel=None, wait=False):
+        # We wont be using wait/channel, you can implement them yourself. This is to match the MenuPages signature.
+        await self._source._prepare_once()
+        self.ctx = ctx
+        self.message = await self.send_initial_message(ctx, ctx.channel)
+
+    async def _get_kwargs_from_page(self, page):
+        """This method calls ListPageSource.format_page class"""
+        value = await super()._get_kwargs_from_page(page)
+        if 'view' not in value:
+            value.update({'view': self})
+        return value
+
+    async def interaction_check(self, interaction):
+        """Only allow the author that invoke the command to be able to use the interaction"""
+        return interaction.user == self.ctx.author
+
+    # This is extremely similar to Custom MenuPages(I will not explain these)
+    @ui.button(emoji='<:before_fast_check:754948796139569224>', style=discord.ButtonStyle.blurple)
+    async def first_page(self, button, interaction):
+        await self.show_page(0)
+
+    @ui.button(emoji='<:before_check:754948796487565332>', style=discord.ButtonStyle.blurple)
+    async def before_page(self, button, interaction):
+        await self.show_checked_page(self.current_page - 1)
+
+    @ui.button(emoji='<:stop_check:754948796365930517>', style=discord.ButtonStyle.blurple)
+    async def stop_page(self, button, interaction):
+        self.stop()
+
+    @ui.button(emoji='<:next_check:754948796361736213>', style=discord.ButtonStyle.blurple)
+    async def next_page(self, button, interaction):
+        await self.show_checked_page(self.current_page + 1)
+
+    @ui.button(emoji='<:next_fast_check:754948796391227442>', style=discord.ButtonStyle.blurple)
+    async def last_page(self, button, interaction):
+        await self.show_page(self._source.get_max_pages() - 1)
 
 class misc(commands.Cog):
 
@@ -18,33 +79,22 @@ class misc(commands.Cog):
         i = 0
         for guild in self.client.guilds:
             i+=1
-            e = f"{str(i)} - {guild.name} : {guild.id}"
+            e = f"` {str(i)} ` - {guild.name} : `{guild.id}`"
             serverlist.append(e)
 
-        button_left = Button(emoji='<:RedLeft:941266906462191626>')
-        button_right = Button(emoji='<:RedRight:941266864330408016>')
-
-
-        view = View()
-        view.add_item(button_left)
-        view.add_item(button_right)
-
         total_servers = len(serverlist)
-        serverlist = [serverlist[i:i+total_servers//2] for i in range(0,total_servers,total_servers//2)]
-        serverlist1 = serverlist[0]
-        serverlist2 = serverlist[1]
-        serverlist1 = "\n".join(serverlist1)
-        serverlist2 = "\n".join(serverlist2)
-        embed1 = discord.Embed(title = f"Total Servers : {total_servers}",description = f"{serverlist1}",color = discord.Colour.gold())
-        embed2 = discord.Embed(title = f"Total Servers : {total_servers}",description = f"{serverlist2}",color = discord.Colour.gold())
-        async def button_callback1(interaction):
-            await interaction.response.edit_message(embed = embed2)
-        async def button_callback2(interaction):
-            await interaction.response.edit_message(embed = embed1)
+        serverlist = [serverlist[i:i+10] for i in range(0,total_servers,10)]
+        await ctx.send(serverlist)
 
-        button_right.callback = button_callback1
-        button_left.callback = button_callback2
-        await ctx.send(embed=embed1,view = view)
+        serverlist_strings = []
+        for i in range(len(serverlist)):
+            await ctx.send(serverlist[i])
+            server_strings = "\n".join(serverlist[i])
+            serverlist_strings.append(server_strings)
+
+        formatter = MySource(serverlist_strings, per_page=1)
+        menu = MyMenuPages(formatter)
+        await menu.start(ctx)
 
     @commands.command()
     async def ping(self,ctx):
@@ -58,17 +108,9 @@ class misc(commands.Cog):
 
     @commands.command(name ='stats',pass_context = True,case_insensitive = True,aliases = ['st'])
     async def stats(self,ctx):
-        url = 'https://panel.epikhost.xyz/api/client/servers/b0b9e335/resources'
-        headers = {
-            "Authorization": "Bearer vD6wPREn7IcJdMKh0DYu70OPPLZRzpZ7VPfRXWLnLskXtOSC",
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-        response = requests.request('GET', url, headers=headers)
-        response = json.loads(response.text)
-        memory_usage = int(response['attributes']['resources']['memory_bytes'])//1024**2
-        disk_usage = int(response['attributes']['resources']['disk_bytes'])//1024**2
-        cpu_usage = round(float(response['attributes']['resources']['cpu_absolute']),2)
+        memory_usage = psutil.virtual_memory()[3]>>20
+        memory_total = psutil.virtual_memory()[0]>>20
+        cpu_usage = psutil.cpu_percent(1)
 
         member_count = sum(guild.member_count for guild in self.client.guilds)
 
@@ -79,8 +121,7 @@ Total Users : {member_count}
 ''')    
         embed.add_field(name="**__System Stats__**",value = f'''
 CPU : {cpu_usage}% Used
-RAM : {memory_usage}/500 MB Used
-DISK : {disk_usage}/500 MB Used
+RAM : {memory_usage}/{memory_total} Used
 ''')
         embed.set_footer(text = "Made With ❤️ | By AE・ARTHISHᵍᶠˣ#2716")
 
