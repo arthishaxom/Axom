de0c1280c9f1092bfa040f876405685e72ab4c74
arthishaxom <arthishgfx@gmail.com>
2022-03-10 09:32:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ import json
 import asyncpg
 import jishaku
 
-with open("config.json",'r') as configjsonFile:
-    configData=json.load(configjsonFile)
-    TOKEN=configData["DISCORD_TOKEN"]
+with open("config.json", 'r') as configjsonFile:
+    configData = json.load(configjsonFile)
+    TOKEN = configData["DISCORD_TOKEN"]
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 times_used = 0
-activity = discord.Activity(type = discord.ActivityType.watching, name="Leaderboards & Points")
-client = commands.Bot(command_prefix='&',activity=activity,strip_after_prefix = True,owner_id = 315342835283001344,intents = intents)
+activity = discord.Activity(
+    type=discord.ActivityType.watching, name="Leaderboards & Points")
+client = commands.Bot(command_prefix='&', activity=activity,
+                      strip_after_prefix=True, owner_id=315342835283001344, intents=intents)
 client.remove_command("help")
-
 
 
 # async def create_pool():
@@ -31,13 +32,14 @@ client.remove_command("help")
 
 @client.command()
 @commands.is_owner()
-async def loadcog(ctx,extension):
+async def loadcog(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send("DONE")
 
+
 @client.command()
 @commands.is_owner()
-async def unloadcog(ctx,extension):
+async def unloadcog(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send("DONE")
 
@@ -46,12 +48,17 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 client.load_extension('jishaku')
+os.environ["JISHAKU_NO_UNDERSCORE"] = "t"
+os.environ["JISHAKU_FORCE_PAGINATOR"] = "t"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "t"
+
 
 @client.command()
 async def test(ctx, arg):
     msg = await ctx.send(arg)
+
     def check(msg):
-            return msg.author == ctx.author and msg.channel == ctx.channel
+        return msg.author == ctx.author and msg.channel == ctx.channel
     await msg.add_reaction("✅")
 
 client.run(TOKEN)
