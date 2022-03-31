168d53f992ed4501ff6c70ef7813ace3a8b62e38
arthishaxom <arthishgfx@gmail.com>
2022-03-31 16:15:29 +0530

    Buttons Update

--- Utilities/buttons_view.py
+++ Utilities/buttons_view.py
@@ -3,60 +3,23 @@ import discord
 from discord.ext import commands
 
 
-class MyView(View):
+class ConfirmView(View):
     def __init__(self, ctx):
         super().__init__(timeout=60)
         self.ctx = ctx
         self.value = None
 
     @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
-    async def button_callback(self, button, interaction):
+    async def button_callback(self, interaction, button):
         await interaction.response.edit_message(view=self)
         self.value = "Yes"
         self.stop()
 
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-    async def no_button_callback(self, button, interaction):
+    async def no_button_callback(self, interaction, button):
         self.clear_items()
         await interaction.response.edit_message(view=self)
         self.value = "No"
-        self.stop()
-
-    async def on_timeout(self):
-        return
-
-
-class CusView(View):
-    def __init__(self, ctx):
-        super().__init__(timeout=60)
-        self.ctx = ctx
-        self.value = None
-
-    @discord.ui.button(label="A", style=discord.ButtonStyle.grey)
-    async def button_callback(self, button, interaction):
-        await interaction.response.edit_message(view=self)
-        self.value = "A"
-        self.stop()
-
-    @discord.ui.button(label="B", style=discord.ButtonStyle.grey)
-    async def no_button_callback(self, button, interaction):
-        self.clear_items()
-        await interaction.response.edit_message(view=self)
-        self.value = "B"
-        self.stop()
-
-    @discord.ui.button(label="C", style=discord.ButtonStyle.grey)
-    async def no_button_callback(self, button, interaction):
-        self.clear_items()
-        await interaction.response.edit_message(view=self)
-        self.value = "C"
-        self.stop()
-
-    @discord.ui.button(label="D", style=discord.ButtonStyle.grey)
-    async def no_button_callback(self, button, interaction):
-        self.clear_items()
-        await interaction.response.edit_message(view=self)
-        self.value = "D"
         self.stop()
 
     async def on_timeout(self):
@@ -70,13 +33,13 @@ class MyView(View):
         self.value = None
 
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
-    async def button_callback(self, button, interaction):
+    async def button_callback(self, interaction, button):
         await interaction.response.edit_message(view=self)
         self.value = "Yes"
         self.stop()
 
     @discord.ui.button(label="No", style=discord.ButtonStyle.red)
-    async def no_button_callback(self, button, interaction):
+    async def no_button_callback(self, interaction, button):
         self.clear_items()
         await interaction.response.edit_message(view=self)
         self.value = "No"
